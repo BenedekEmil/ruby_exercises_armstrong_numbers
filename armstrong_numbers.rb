0a2c8c240f7da477b27574e30b04ff192cc189c0
Emil Benedek <emil.benedek@unifiedpost.com>
2019-09-27 11:52:04 +0300

    improved

--- armstrong_numbers.rb
+++ armstrong_numbers.rb
@@ -1,15 +1,5 @@
 module ArmstrongNumbers
   def self.include?(number)
-    number == armstrong_sum(number.to_i.digits,
-                            0,
-                            number.digits.length)
+    number == number.digits.inject(0) { |sum, i| sum + i**number.digits.length}
   end
-
-  def self.armstrong_sum(digits_array, sum, length)
-    sum += digits_array[0]**length
-    return sum if digits_array.length == 1
-
-    armstrong_sum(digits_array.drop(1), sum, length)
-  end
-  private_class_method :armstrong_sum
 end
